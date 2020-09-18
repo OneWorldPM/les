237d3a50d90eca2b6e12e48eaf03a573e7be311b
vishalsavaliya <vishalsavaliya3@gmail.com>
2020-09-18 17:07:54 +0530

    Update

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session','common','xmlrpc');
+$autoload['libraries'] = array('database', 'session','Common','xmlrpc');
 
 /*
 | -------------------------------------------------------------------
