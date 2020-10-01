e9d459a6a70bc1c40112a2f9f66b1359ee55279f
athulak <athullive@gmail.com>
2020-10-02 00:44:48 +0200

    Lounge - All required changes

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -34,6 +34,7 @@ class Login extends CI_Controller {
                 $session = array(
                     'cid' => $data['cust_id'],
                     'cname' => $data['first_name'],
+                    'fullname' => $data['first_name'].' '.$data['last_name'],
                     'email' => $data['email'],
                     'token' => $token,
                     'userType' => 'user'
@@ -54,6 +55,7 @@ class Login extends CI_Controller {
             $session = array(
                 'cid' => $data['cust_id'],
                 'cname' => $data['first_name'],
+                'fullname' => $data['first_name'].' '.$data['last_name'],
                 'email' => $data['email'],
                 'token' => $token,
                 'userType' => 'user'
