9f8edbd0d954ef7b8a5fce90db4dd9759da81164
athulak <athullive@gmail.com>
2020-10-02 16:39:55 +0200

    Icons colour change

--- application/views/lounge/meet.php
+++ application/views/lounge/meet.php
@@ -33,7 +33,9 @@ if ($meeting_status['status'] == false)
             <div class="feed-control-icons" style="display: inline;">
 
                 <div class="mute-mic-btn" style="display: inline;">
-                    <i class="fa fa-microphone-slash fa-3x mute-mic-btn-icon" aria-hidden="true" style="color:#ff422b;"></i>
+<!--                    <i class="fa fa-microphone-slash fa-3x mute-mic-btn-icon" aria-hidden="true" style="color:#ff422b;"></i>-->
+                    <i class="fa fa-microphone fa-3x mute-mic-btn-icon" aria-hidden="true" style="color:#12b81c;"></i>
+
                 </div>
 
                 <div class="share-screen-btn" style="display: inline;">
@@ -41,7 +43,7 @@ if ($meeting_status['status'] == false)
                 </div>
 
                 <div class="leave-btn" style="display: inline;">
-                    <i class="fa fa-sign-out fa-3x leave-btn-icon" aria-hidden="true" style="color:#6f8de3;"></i>
+                    <i class="fa fa-sign-out fa-3x leave-btn-icon" aria-hidden="true" style="color:#e36f7a;"></i>
                 </div>
             </div>
         </div>
