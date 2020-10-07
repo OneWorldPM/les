81a098d2af687dce10b97f62c40f1df297d094cf
athulak <athullive@gmail.com>
2020-10-07 17:17:42 +0200

    Sorry message removal

--- application/views/lounge/meet.php
+++ application/views/lounge/meet.php
@@ -5,7 +5,7 @@ if ($meeting_status['status'] == false)
     <main role="main" class="container" style="text-align: center;">
 
         <div class="starter-template">
-            <h1>Sorry!</h1>
+<!--            <h1>Sorry!</h1>-->
             <p class="lead"><?=$meeting_status['message']?></p>
         </div>
 
