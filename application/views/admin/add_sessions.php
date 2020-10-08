1d93116a9e1b2ccc0a007aee432ff25d3f9cc8b8
vishalsavaliya <vishalsavaliya3@gmail.com>
2020-10-08 17:03:32 +0530

    Add Session Update

--- application/views/admin/add_sessions.php
+++ application/views/admin/add_sessions.php
@@ -208,8 +208,13 @@
                                                             <div class ='form-group'>
                                                                 <label> Resources Uploads</label>
                                                                 <input type ='file' class='form-control' name='presenter_resource[]' id='presenter_resource'>
-                                                                <?php if($value->presenter_resource != ""){ ?>
+                                                                <?php if($value->presenter_resource != ""){ 
+                                                                    if(file_exists("./uploads/presenter_resource/".$value->presenter_resource)){
+                                                                    ?>
                                                                 <img src="<?= base_url() ?>uploads/presenter_resource/<?= $value->presenter_resource ?>" style="height: 100px; width: 100px;">
+                                                                    <?php } else { ?>
+                                                                 <img src="<?= base_url() ?>front_assets/images/session_avtar.jpg" style="height: 100px; width: 100px;">
+                                                                    <?php } ?>
                                                                 <?php }else{?>
                                                                 <img src="<?= base_url() ?>front_assets/images/session_avtar.jpg" style="height: 100px; width: 100px;">
                                                                 <?php } ?>
