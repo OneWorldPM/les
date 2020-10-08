0b0b03bf3f056091c5482f5c7467788981248a0d
hayreddintuzel <hayreddintuzel@gmail.com>
2020-10-08 11:04:23 +0300

    null for custom query

--- application/models/user/SupportChat_Model.php
+++ application/models/user/SupportChat_Model.php
@@ -28,17 +28,22 @@ class SupportChat_Model extends CI_Model
 
     function getAllChats($attendee_id)
     {
-        $this->db->select(
-            "
-            sc.*, 
-            CONCAT(cm.first_name, ' ', cm.last_name) AS attendee_name
-            "
-        );
-        $this->db->from('support_chat sc');
-        $this->db->join('customer_master cm', 'cm.cust_id = sc.attendee_id', 'left');
-        $this->db->where('sc.attendee_id', $attendee_id);
-        $this->db->order_by('sc.datetime','asc');
-        $query = $this->db->get();
+        // $this->db->select(
+        //     "
+        //     sc.*, 
+        //     CONCAT(cm.first_name, ' ', cm.last_name) AS attendee_name
+        //     "
+        // );
+        // $this->db->from('support_chat sc');
+        // $this->db->join('customer_master cm', 'cm.cust_id = sc.attendee_id', 'left');
+        // $this->db->where('sc.attendee_id', $attendee_id);
+        // $this->db->order_by('sc.datetime','asc');
+        $query = $this->db->query("select support_chat.*,CONCAT(customer_master.first_name, ' ', customer_master.last_name) AS attendee_name from support_chat 
+        left join customer_master 
+        on customer_master.cust_id=support_chat.attendee_id 
+        where support_chat.attendee_id='$attendee_id'
+        order by support_chat.datetime asc");
+
         if($query->num_rows() != 0)
         {
             return $query->result_array();
