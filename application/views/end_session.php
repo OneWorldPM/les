ba13099229fdefd63752bcb94c10ada7ac2050f0
hayreddintuzel <hayreddintuzel@gmail.com>
2020-10-12 16:08:51 +0300

    center

--- application/views/end_session.php
+++ application/views/end_session.php
@@ -2,12 +2,18 @@
     .jumbotron{
         width: 900px;
         margin: 50px auto;
+        text-align: center;
+        max-width: 100%;
     }
+    .jumbotron p{
+     width: 100%;
+    }
+
 </style>
 <div class="container">
     <div class="jumbotron">
         <h1>Session is over</h1>
-        <p>Thank you for Attending Page</p>
+        <p>Thank you for Attending</p>
         <p><a class="btn btn-primary btn-lg" href="<?=base_url()."home"?>" role="button">Go Homepage</a></p>
     </div>
 </div>
