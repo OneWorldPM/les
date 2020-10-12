b208670a2cdfc90b7628f16807a5ea7eab889985
Vishal Savaliya <vishalsavaliya3@gmail.com>
2020-10-12 19:42:32 +0530

    iframe

--- application/views/sessions.php
+++ application/views/sessions.php
@@ -141,8 +141,8 @@
         <!-- CONTENT -->
         <section class="content">
             <div>
-                <div class="videoWindow">
-                    <iframe class="embed-responsive-item" src="https://player.vimeo.com/video/467360653" allowfullscreen></iframe>
+                <div>
+                    <iframe src="https://player.vimeo.com/video/467360653" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
                 </div>
 
                 <!-- Blog post-->
