0407a381193efb0ea30f01d14060897a77f67322
hayreddintuzel <hayreddintuzel@gmail.com>
2020-10-12 15:40:53 +0300

    thank you attending

---
+++ application/views/end_session.php
@@ -0,0 +1,13 @@
+<style>
+    .jumbotron{
+        width: 900px;
+        margin: 50px auto;
+    }
+</style>
+<div class="container">
+    <div class="jumbotron">
+        <h1>Session is over</h1>
+        <p>Thank you for Attending Page</p>
+        <p><a class="btn btn-primary btn-lg" href="<?=base_url()."home"?>" role="button">Go Homepage</a></p>
+    </div>
+</div>
