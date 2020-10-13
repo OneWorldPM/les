87b230a88c919d309297c70c6670b2c4459e2de7
vishalsavaliya <vishalsavaliya3@gmail.com>
2020-10-13 11:58:53 +0530

    LES Create New Page Agenda

---
+++ application/views/agenda.php
@@ -0,0 +1,34 @@
+<style>
+    thead tr th{
+        text-align: center;
+    }
+    tbody tr td{
+        text-align: center;
+    }
+    .files{
+        width: 1300px;
+        max-width: 100%;
+        margin: 0 auto;
+    }
+    .files img{
+        object-fit: contain;
+        width: 90px;
+        height: 70px;
+    }
+    .files td{
+        vertical-align: middle !important;
+    }
+</style>
+<div class="parallax">
+    <h1 style="position: absolute;  margin-top: 15%; margin-left: 40%; font-weight: bold;">Schedule At A Glance</h1>
+    <img src="<?= base_url() ?>front_assets/images/2020_LES_Annual_Meeting_Schedule_At_A_Glance.jpg" style="width: 100%;"/>
+</div>
+<!--<section class="parallax fullscreen" style="background-image: url(<?= base_url() ?>front_assets/images/2020_LES_Annual_Meeting_Schedule_At_A_Glance.jpg); top: 20; padding-top: 20px;">
+    <div class="container text-middle">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Schedule At A Glance</h1>
+            </div>
+        </div>
+    </div>
+</section>-->
