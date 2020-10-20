9c7b500385c158057ea4f7c52e57c66de9bb6ac2
vishalsavaliya <vishalsavaliya3@gmail.com>
2020-10-20 15:57:09 +0530

    sessions set video

--- application/views/sessions.php
+++ application/views/sessions.php
@@ -173,11 +173,13 @@
                         <iframe class="embed-responsive-item" src="https://player.vimeo.com/video/467905182" allowfullscreen></iframe>
                     </div>	
 				 <?php }elseif ($session_date_from_url == '2020-10-20' && date("Y-m-d") == '2020-10-20'){ ?>
-		            <div class="col-md-6>">
-					  <img alt="" src="<?= base_url() ?>front_assets/images/thanks.jpg">
+		             <div class="col-md-12>">
+					<div class="col-md-6>">
+					  <img alt="" src="<?= base_url() ?>front_assets/images/thanks.png">
 					</div>
 					<div class="col-md-6>">
 					<iframe src="https://player.vimeo.com/video/467905251?autoplay=1&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+					</div>
 					</div>
 				<?php } ?>
 
