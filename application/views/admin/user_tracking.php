1f8af0ed42ba5a7752cd6f1f61f9eb41387aa7f0
vishalsavaliya <vishalsavaliya3@gmail.com>
2020-10-29 10:06:03 +0530

    sponsor pages activity

--- application/views/admin/user_tracking.php
+++ application/views/admin/user_tracking.php
@@ -2,11 +2,9 @@
 <script src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
 
 <?php
-if (isset($booth_tracking) && !empty($booth_tracking))
-{
+if (isset($booth_tracking) && !empty($booth_tracking)) {
     $unique_list = [];
-    foreach ($booth_tracking as $val)
-    {
+    foreach ($booth_tracking as $val) {
         $unique_list[] = $val->cust_id;
     }
 
@@ -77,50 +75,49 @@ if (isset($booth_tracking) && !empty($booth_tracking))
                             <div class="col-md-12 table-responsive">
                                 <table id="example" class="table table-bordered table-striped text-center ">
                                     <thead class="th_center">
-                                    <tr>
-                                        <th>Sponsor</th>
-                                        <th>Action</th>
-                                        <th>Addnl Info</th>
-                                        <th>Full Name</th>
-                                        <th>Phone No.</th>
-                                        <th>Email</th>
-                                        <th>Entry Time</th>
-                                        <th>End Time</th>
-                                        <th>Total Time</th>
-                                    </tr>
+                                        <tr>
+                                            <th>Sponsor</th>
+                                            <th>Action</th>
+                                            <th>Addnl Info</th>
+                                            <th>Full Name</th>
+                                            <th>Phone No.</th>
+                                            <th>Email</th>
+                                            <th>Entry Time</th>
+                                            <th>End Time</th>
+                                            <th>Total Time</th>
+                                        </tr>
                                     </thead>
                                     <tbody>
-                                    <?php
-                                    if (isset($booth_tracking) && !empty($booth_tracking)) {
-                                        foreach ($booth_tracking as $val) {
-                                            if ($val->end_date_time != '')
-                                            {
-                                                $totalTime = date('H:i:s', mktime(0, 0, (strtotime($val->end_date_time)- strtotime($val->start_date_time))));
-                                            }else{
-                                                $totalTime = "-";
-                                            }
-                                            ?>
-                                            <tr>
-                                                <td><?= $val->company_name ?></td>
-                                                <td><?= $val->action ?></td>
-                                                <td><?= $val->addnl_info ?></td>
-                                                <td><?= $val->first_name . ' ' . $val->last_name ?></td>
-                                                <td><?= $val->phone ?></td>
-                                                <td><?= $val->email ?></td>
-                                                <td><?= date("Y-m-d h:i:s", strtotime($val->start_date_time)) ?></td>
-                                                <td>
-                                                    <?php if ($val->end_date_time != '') { ?>
-                                                        <?= date("Y-m-d h:i:s", strtotime($val->end_date_time)) ?>
-                                                    <?php } else { ?>
-                                                        -
-                                                    <?php } ?>
-                                                </td>
-                                                <td><?=$totalTime?></td>
-                                            </tr>
-                                            <?php
-                                        }
-                                    }
-                                    ?>
+<?php
+if (isset($booth_tracking) && !empty($booth_tracking)) {
+    foreach ($booth_tracking as $val) {
+        if ($val->end_date_time != '') {
+            $totalTime = date('H:i:s', mktime(0, 0, (strtotime($val->end_date_time) - strtotime($val->start_date_time))));
+        } else {
+            $totalTime = "-";
+        }
+        ?>
+                                                <tr>
+                                                    <td><?= $val->company_name ?></td>
+                                                    <td><?= $val->action ?></td>
+                                                    <td><?= $val->addnl_info ?></td>
+                                                    <td><?= $val->first_name . ' ' . $val->last_name ?></td>
+                                                    <td><?= $val->phone ?></td>
+                                                    <td><?= $val->email ?></td>
+                                                    <td><?= date("Y-m-d h:i:s", strtotime($val->start_date_time)) ?></td>
+                                                    <td>
+        <?php if ($val->end_date_time != '') { ?>
+            <?= date("Y-m-d h:i:s", strtotime($val->end_date_time)) ?>
+                                                        <?php } else { ?>
+                                                            -
+                                                        <?php } ?>
+                                                    </td>
+                                                    <td><?= $totalTime ?></td>
+                                                </tr>
+        <?php
+    }
+}
+?>
                                     </tbody>
                                 </table>
                             </div>
@@ -134,46 +131,36 @@ if (isset($booth_tracking) && !empty($booth_tracking))
 </div>
 
 <script>
-$(document).ready(function () {
+    $(document).ready(function () {
 
-    $('#example thead th').each( function () {
-        var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-    } );
-    var trackingTable = $('#example').DataTable({
-        "order": [[ 6, "desc" ]],
-        "dom": '<"top"i>rt<"bottom"flp><"clear">',
-        initComplete: function () {
-            // Apply the search
-            this.api().columns().every( function () {
-                var that = this;
+        $('#example thead th').each(function () {
+            var title = $(this).text();
+            $(this).html('<b>'+ title +'</b><input type="text" placeholder="Search ' + title + '" />');
+        });
+        var trackingTable = $('#example').DataTable({
+            "order": [[6, "desc"]],
+            "dom": 'Bfrtip',
+            "buttons": [
+                'csv'
+            ],
+            initComplete: function () {
+                // Apply the search
+                this.api().columns().every(function () {
+                    var that = this;
 
-                $( 'input', this.header() ).on( 'keyup change clear', function () {
-                    if ( that.search() !== this.value ) {
-                        that
-                            .search( this.value )
-                            .draw();
-                    }
-                } );
-            } );
-        }
-    });
-
-
+                    $('input', this.header()).on('keyup change clear', function () {
+                        if (that.search() !== this.value) {
+                            that
+                                    .search(this.value)
+                                    .draw();
+                        }
+                    });
+                });
+            }
+        });
+  $('.buttons-csv').text('Export CSV');
     });
 </script>
 <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
-<script>
-    $(document).ready(function () {
-        $('#example').DataTable({
-            dom: 'Bfrtip',
-            buttons: [
-                'csv'
-            ]
-        });
-        $('.buttons-csv').text('Export CSV');
-    });
-</script>
-
