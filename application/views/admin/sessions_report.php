b7e4565267cbf522573a324ae6ce261f4c58979d
vishalsavaliya <vishalsavaliya3@gmail.com>
2020-10-29 17:14:25 +0530

    SESSIONS REPORT

--- application/views/admin/sessions_report.php
+++ application/views/admin/sessions_report.php
@@ -1,3 +1,9 @@
+<style>
+    #example_wrapper .dt-buttons .buttons-csv{
+        background-color: #1fbba6;
+        padding: 5px 15px 5px 15px;
+    }
+</style>
 <div class="main-content">
     <div class="wrap-content container" id="container">
         <!-- start: PAGE TITLE -->
@@ -19,16 +25,16 @@
                     <div class="panel-body bg-white" style="border: 1px solid #b2b7bb!important;">
                         <div class="row">
                             <div class="col-md-12 table-responsive">
-                                <table class="table table-bordered table-striped text-center " id="sessions_table">
+                                <table class="table table-bordered table-striped text-center " id="example">
                                     <thead class="th_center">
                                         <tr>
                                             <th>Date</th>
                                             <th>Presenter</th>
                                             <th>User</th>
-											 <th>Phone Number</th>
+                                            <th>Phone Number</th>
                                             <th>Company</th>
                                             <th>City</th>
-                                             <th>Session Title</th>
+                                            <th>Session Title</th>
                                             <th>Question</th>
                                             <th>Answer</th>
                                         </tr>
@@ -40,9 +46,9 @@
                                                 ?>
                                                 <tr>
                                                     <td><?= date("Y-m-d", strtotime($val->reg_question_date)) ?></td>
-                                                     <td><?= $val->presenter_name ?></td>
-                                                    <td><?= $val->first_name." ".$val->last_name ?></td>
-													  <td><?= $val->phone ?></td>
+                                                    <td><?= $val->presenter_name ?></td>
+                                                    <td><?= $val->first_name . " " . $val->last_name ?></td>
+                                                    <td><?= $val->phone ?></td>
                                                     <td><?= $val->company_name ?></td>
                                                     <td><?= $val->city ?></td>
                                                     <td><?= $val->session_title ?></td>
@@ -65,10 +71,17 @@
     </div>
 </div>
 </div>
+<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function () {
-        $("#sessions_table").dataTable({
-            "ordering": false,
+        $("#example").dataTable({
+            "dom": 'Bfrtip',
+            "buttons": [
+                'csv'
+            ],
         });
+        $('.buttons-csv').text('Export CSV');
     });
 </script>
