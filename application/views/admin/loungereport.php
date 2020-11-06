59b3d12c40dabce8922d16698706c6ba58d6a625
vishalsavaliya <vishalsavaliya3@gmail.com>
2020-11-06 15:27:00 +0530

    Lounge Report

--- application/views/admin/loungereport.php
+++ application/views/admin/loungereport.php
@@ -112,7 +112,6 @@ if (isset($loungereport) && !empty($loungereport)) {
             $(this).html('<b>' + title + '</b><br><input type="text" placeholder="Search ' + title + '" />');
         });
         var trackingTable = $('#example').DataTable({
-            "order": [[4, "desc"]],
             "dom": 'Bfrtip',
             "buttons": [
                 'csv'
